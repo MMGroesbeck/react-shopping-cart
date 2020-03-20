30e565d9844eac86a0a7a585fc250a6aebb0ef12
M Groesbeck <m.groesbeck@gmail.com>
2020-03-20 07:31:31 -0700

    Provider set up, but child components cannot see state?

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, createContext } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
@@ -7,27 +7,33 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+export const ShopContext = createContext();
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
-
 	const addItem = item => {
 		// add the given item to the cart
 		setCart([...cart, item]);
 	};
+	const removeItem = item => {
+		setCart(cart.filter(i => i.id !== item.id));
+	}
 
 	return (
 		<div className="App">
+			<ShopContext.Provider value={products, cart, addItem, removeItem}>
 			<Navigation cart={cart} />
 
 			{/* Routes */}
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products />
 			</Route>
 
 			<Route path="/cart">
-				<ShoppingCart cart={cart} />
+				<ShoppingCart />
 			</Route>
+			</ShopContext.Provider>
 		</div>
 	);
 }
--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 const Item = props => {
+	const handleRemove = e => {
+		e.preventDefault();
+		props.removeItem({id: props.id});
+	}
 	return (
 		<div className="shopping-cart_item">
 			<img src={props.image} alt={`${props.title} book`} />
@@ -9,7 +13,7 @@ const Item = props => {
 			<div>
 				<h1>{props.title}</h1>
 				<p>$ {props.price}</p>
-				<button>Remove from cart</button>
+				<button onClick={handleRemove}>Remove from cart</button>
 			</div>
 		</div>
 	);
